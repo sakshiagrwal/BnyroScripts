89b88ff5bfdaa3ca73f319f51b2c2ce02d931813
Bnyro <bnyro@tutanota.com>
2023-01-17 19:44:53 +0100

    Update the program structure

---
+++ programs.sh
@@ -0,0 +1,48 @@
+DESKTOPAPPS="thunderbird gimp inkscape"
+AURPKGS="librewolf-bin android-studio-beta vscodium-bin"
+CMDTOOLS="
+	fish
+	helix
+	git
+	wget
+	exa
+	bat
+	duf
+	gdu
+	bottom
+	xh
+	zoxide
+	tealdeer
+	onefetch
+	android-tools
+	yt-dlp
+	ffmpeg
+"
+
+GTKTHEMES="
+	https://github.com/vinceliuice/Orchis-theme
+	https://github.com/vinceliuice/Colloid-gtk-theme
+	https://github.com/vinceliuice/Layan-gtk-theme
+	https://github.com/vinceliuice/WhiteSur-gtk-theme
+	https://github.com/EliverLara/Nordic
+  https://github.com/ZorinOS/zorin-desktop-themes
+	https://github.com/vinceliuice/Graphite-gtk-theme
+"
+
+ICONTHEMES="
+	https://github.com/vinceliuice/Tela-circle-icon-theme
+	https://github.com/yeyushengfan258/Reversal-icon-theme
+	https://github.com/vinceliuice/Qogir-icon-theme
+	https://github.com/vinceliuice/WhiteSur-icon-theme
+	https://github.com/EliverLara/candy-icons
+	https://github.com/gvolpe/BeautyLine
+  https://github.com/ZorinOS/zorin-icon-themes
+	https://github.com/PapirusDevelopmentTeam/papirus-icon-theme
+"
+
+GRUBTHEMES="
+	https://github.com/vinceliuice/grub2-themes
+	https://github.com/sandesh236/sleek--themes
+	https://github.com/AdisonCavani/distro-grub-themes
+"
+
--- setup.sh
+++ setup.sh
@@ -4,36 +4,7 @@ THEMESDIR=/usr/share/themes
 ICONSDIR=/usr/share/icons
 GRUBDIR=/boot/grub/themes
 
-DESKTOPAPPS="thunderbird gimp inkscape"
-AURPKGS="librewolf-bin android-studio-beta vscodium-bin"
-CMDTOOLS="fish helix git wget exa bat duf gdu bottom xh zoxide tldr onefetch android-tools yt-dlp ffmpeg"
-
-GTKTHEMES="
-	https://github.com/vinceliuice/Orchis-theme
-	https://github.com/vinceliuice/Colloid-gtk-theme
-	https://github.com/vinceliuice/Layan-gtk-theme
-	https://github.com/vinceliuice/WhiteSur-gtk-theme
-	https://github.com/EliverLara/Nordic
-  https://github.com/ZorinOS/zorin-desktop-themes
-	https://github.com/vinceliuice/Graphite-gtk-theme
-"
-
-ICONTHEMES="
-	https://github.com/vinceliuice/Tela-circle-icon-theme
-	https://github.com/yeyushengfan258/Reversal-icon-theme
-	https://github.com/vinceliuice/Qogir-icon-theme
-	https://github.com/vinceliuice/WhiteSur-icon-theme
-	https://github.com/EliverLara/candy-icons
-	https://github.com/gvolpe/BeautyLine
-  https://github.com/ZorinOS/zorin-icon-themes
-	https://github.com/PapirusDevelopmentTeam/papirus-icon-theme
-"
-
-GRUBTHEMES="
-	https://github.com/vinceliuice/grub2-themes
-	https://github.com/sandesh236/sleek--themes
-	https://github.com/AdisonCavani/distro-grub-themes
-"
+source "$0"/programs.sh
 
 has() {
 	(command -v "$1" &>/dev/null)
