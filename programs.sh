f185348cc9a6f019f17f32330a0a357b64fc8977
Bnyro <bnyro@tutanota.com>
2023-02-05 11:34:36 +0100

    Add fzf

--- programs.sh
+++ programs.sh
@@ -1,5 +1,5 @@
 DESKTOPAPPS="thunderbird gimp inkscape"
-AURPKGS="librewolf-bin android-studio-beta vscodium-bin"
+AURPKGS="librewolf-bin android-studio-beta vscodium-bin android-tools"
 CMDTOOLS="
 	fish
 	helix
@@ -19,7 +19,7 @@ CMDTOOLS="
 	tokei
 	miniserve
 	pastel
-	android-tools
+	fzf
 	yt-dlp
 	ffmpeg
 "
